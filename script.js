0c5c89791c802fa0f66dc4dedc1f94d14a6fe1c9
Alpha-NAYRA <aryanedu5786@GMAIL.COM>
2022-03-30 23:54:06 +0530

    added functionality

--- script.js
+++ script.js
@@ -4,25 +4,42 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   const data = {
     "feilds": [{
-        "name": "email",
-        "value": form.elements['email'].value]
+      "name": "email",
+      "value": form.elements['email'].value
     }],
-    "context":{
+    "context": {
       "pageUrl": "https://www.toplyne.io/",
-      "pageName":"Home Page Top MQL"
+      "pageName": "Home Page Top MQL"
     }
   }
+
+  const url = "https://api.hsforms.com/submissions/v3/integration/submit/25336449/5f662e82-7b62-41ba-bf1e-a062a9516124";
+
   const sendEmailData = async () => {
-    const response = await fetch("url", {
+    const response = await fetch(url, {
       method: "POST",
-      body: JSON.stringify(cart),
+      body: JSON.stringify(data),
     });
 
     if (!response.ok) {
       throw new Error('Unable to register');
+    } else {
+      location.replace("https://www.toplyne.io/get-started")
     }
   }
-}
+})
 
-const responseData = await response.json();
-});
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   const data = {
+//     "feilds": [{
+//       "name": "email",
+//       "value": form.elements['email'].value
+//     }],
+//     "context": {
+//       "pageUrl": "https://www.toplyne.io/",
+//       "pageName": "Home Page Top MQL"
+//     }
+//   }
+//   location.replace("https://www.toplyne.io/get-started");
+// })
