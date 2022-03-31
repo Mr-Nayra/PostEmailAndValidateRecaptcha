5430a2097f143221abd0007803b3139b93514cab
Alpha-NAYRA <aryanedu5786@GMAIL.COM>
2022-03-31 08:58:12 +0530

    fetches data from api

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const form = document.getElementById('email-form');
 
 form.addEventListener('submit', (event) => {
+  var btn = document.getElementById("button");
+  btn.setAttribute("value", "Please wait...");
+
   event.preventDefault();
   const data = {
-    "feilds": [{
+    "fields": [{
       "name": "email",
-      "value": form.elements['email'].value
+      "value": "a@gmail.com"
     }],
     "context": {
-      "pageUrl": "https://www.toplyne.io/",
+      "pageUri": "https://www.toplyne.io/",
       "pageName": "Home Page Top MQL"
     }
-  }
+  };
 
   const url = "https://api.hsforms.com/submissions/v3/integration/submit/25336449/5f662e82-7b62-41ba-bf1e-a062a9516124";
 
@@ -19,27 +22,18 @@ form.addEventListener('submit', (event) => {
     const response = await fetch(url, {
       method: "POST",
       body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      },
     });
 
     if (!response.ok) {
       throw new Error('Unable to register');
     } else {
+      console.log(response);
       location.replace("https://www.toplyne.io/get-started")
     }
   }
-})
 
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   const data = {
-//     "feilds": [{
-//       "name": "email",
-//       "value": form.elements['email'].value
-//     }],
-//     "context": {
-//       "pageUrl": "https://www.toplyne.io/",
-//       "pageName": "Home Page Top MQL"
-//     }
-//   }
-//   location.replace("https://www.toplyne.io/get-started");
-// })
+  sendEmailData();
+});
